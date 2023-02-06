53622f55ad66479b42439d3cfc953098aaa059fb
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-05 21:46:08 -0500

    injecting services

--- Services/src/app/accounts.service.ts
+++ Services/src/app/accounts.service.ts
@@ -1,3 +1,6 @@
+import {LoggingService} from "./logging.service";
+import {Injectable} from "@angular/core";
+
 export class AccountsService {
   accounts = [
     {
@@ -13,6 +16,9 @@ export class AccountsService {
       status: 'unknown'
     }
   ];
+
+  constructor(private loggingService: LoggingService) {
+  }
 
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
