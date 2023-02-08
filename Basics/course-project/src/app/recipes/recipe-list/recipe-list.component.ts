60370f972b9df8764d4af13b86231765d4c08bca
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-07 23:17:21 -0500

    managing recipes in service

--- Basics/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Basics/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,10 +13,11 @@ export class RecipeListComponent {
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
-  constructor() {
+  constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
