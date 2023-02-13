3cd5d3456d92f52bfce68985c9fc9a7b91b07a12
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-13 16:16:12 -0500

    setup

---
+++ Reactive-Forms/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
