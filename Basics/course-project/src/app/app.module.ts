e220f36d9c00752a3864a6c44f82d9c5b60c7ea4
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-14 23:04:25 -0500

    validation

--- Basics/course-project/src/app/app.module.ts
+++ Basics/course-project/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {FormsModule} from "@angular/forms";
 import {DropdownDirective} from "./shared/dropdown.directive";
