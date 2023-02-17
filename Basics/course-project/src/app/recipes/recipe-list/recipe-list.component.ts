82e5b8c6d987eb84fcb001b4c0ca5b1a2f7889f3
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-17 16:49:05 -0500

    deleting ingredients

--- Basics/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Basics/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,19 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, OnDestroy} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', [])
   ];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService,
               private router: Router,
@@ -19,7 +21,7 @@ export class RecipeListComponent {
   }
 
   ngOnInit() {
-    this.recipeService.recipesChanged.subscribe(
+    this.subscription = this.recipeService.recipesChanged.subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
@@ -29,5 +31,9 @@ export class RecipeListComponent {
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
