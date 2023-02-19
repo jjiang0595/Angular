d9d01fa5d7fad370e35fa0ce0061444e71718ebf
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-18 19:39:26 -0500

    subjects for error handling

--- HTTP/src/app/posts.service.ts
+++ HTTP/src/app/posts.service.ts
@@ -2,19 +2,25 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Post} from "./post.model";
 import {map} from "rxjs/operators";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+  error = new Subject<string>();
+
   constructor(private http: HttpClient) {
   }
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = {title: title, content: content};
-    this.http.post<{ name: string }>('https://ng-complete-guide-da444-default-rtdb.firebaseio.com/posts.json',
-      postData).subscribe(responseData => {
-      console.log(responseData);
-    });
+    this.http
+      .post<{ name: string }>('https://ng-complete-guide-da444-default-rtdb.firebaseio.com/posts.json',
+        postData)
+      .subscribe(responseData => {
+        console.log(responseData);
+      }, error => {
+        this.error.next(error.message);
+      });
   }
 
   fetchPosts(): Observable<Post[]> {
