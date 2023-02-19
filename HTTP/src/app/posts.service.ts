10a70069d0edb422d05d4d6d671672a36051c19e
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-18 20:28:50 -0500

    query params

--- HTTP/src/app/posts.service.ts
+++ HTTP/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Post} from "./post.model";
 import {catchError, map} from "rxjs/operators";
 import {Observable, Subject, throwError} from "rxjs";
@@ -24,10 +24,14 @@ export class PostsService {
   }
 
   fetchPosts(): Observable<Post[]> {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
       .get<{ [key: string]: Post }>('https://ng-complete-guide-da444-default-rtdb.firebaseio.com/posts.json',
         {
           headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+          params: searchParams
         })
       .pipe(map(responseData => {
           const postsArray: Post[] = [];
