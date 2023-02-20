0aba1e087ba2459e613a14f18e6a8fe92fda02df
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-19 21:38:37 -0500

    transforming response data

--- Basics/course-project/src/app/recipes/recipe.service.ts
+++ Basics/course-project/src/app/recipes/recipe.service.ts
@@ -8,18 +8,19 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', [
-        new Ingredient('Meat', 1),
-        new Ingredient("French Fries", 20)
-      ]),
-    new Recipe('Another Test Recipe', 'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('A Test Recipe', 'This is simply a test',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient("French Fries", 20)
+  //     ]),
+  //   new Recipe('Another Test Recipe', 'This is simply a test',
+  //     'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1)
+  //     ]),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {
 
--- Basics/course-project/src/app/shared/data-storage.service.ts
+++ Basics/course-project/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Recipe} from "../recipes/recipe.model";
 import {RecipeService} from "../recipes/recipe.service";
+import {map} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -28,8 +29,13 @@ export class DataStorageService {
       .get<Recipe[]>(
         'https://ng-course-recipe-book-f0059-default-rtdb.firebaseio.com/recipes.json'
       )
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
-      })
+      });
   }
 }
