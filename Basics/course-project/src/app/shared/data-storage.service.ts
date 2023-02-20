523ab5122663fb4cd6938464bf9019ed6d21ff26
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-19 23:07:49 -0500

    resolving data

--- Basics/course-project/src/app/shared/data-storage.service.ts
+++ Basics/course-project/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Recipe} from "../recipes/recipe.model";
 import {RecipeService} from "../recipes/recipe.service";
-import {map} from "rxjs";
+import {map, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -25,17 +25,17 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         'https://ng-course-recipe-book-f0059-default-rtdb.firebaseio.com/recipes.json'
       )
       .pipe(map(recipes => {
-        return recipes.map(recipe => {
-          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-        });
-      }))
-      .subscribe(recipes => {
-        this.recipeService.setRecipes(recipes);
-      });
+            return recipes.map(recipe => {
+              return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+          }), tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
   }
 }
