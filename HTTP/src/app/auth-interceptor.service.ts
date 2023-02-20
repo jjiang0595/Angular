5c6208d42bc574ac2d6aa183c0e1591803637069
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-19 19:44:56 -0500

    Update auth-interceptor.service.ts

--- HTTP/src/app/auth-interceptor.service.ts
+++ HTTP/src/app/auth-interceptor.service.ts
@@ -3,7 +3,6 @@ import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-
     const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')})
     return next.handle(modifiedRequest);
   }
