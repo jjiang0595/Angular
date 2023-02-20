efd545b38be446e3a709581332ba0486f61725ea
Jerry Jiang <jerryjiang49@gmail.com>
2023-02-19 19:54:14 -0500

    setup data storage service

--- Basics/course-project/src/app/app.module.ts
+++ Basics/course-project/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
@@ -37,7 +38,13 @@ import {RecipeService} from "./recipes/recipe.service";
     AppRoutingModule,
     ReactiveFormsModule
   ],
-  providers: [ShoppingListService, RecipeService],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
